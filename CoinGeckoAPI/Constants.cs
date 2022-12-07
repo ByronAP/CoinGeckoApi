2b1006a135df45f0371586d01c6d262f46f54fe3
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-07 12:32:42 -0500

    change const to static readonly

--- CoinGeckoAPI/Constants.cs
+++ CoinGeckoAPI/Constants.cs
@@ -5,20 +5,20 @@
         /// <summary>
         /// The display name of the API provider.
         /// </summary>
-        public const string API_NAME = "CoinGecko";
+        public static readonly string API_NAME = "CoinGecko";
 
         /// <summary>
         /// Actual rate limit is 10-50 RPM
         /// </summary>
-        public const uint API_RATE_LIMIT_MS = 6000;
+        public static readonly uint API_RATE_LIMIT_MS = 6000;
         /// <summary>
         /// Time interval when the rate limit is reset.
         /// AKA: If a response is 429 then let the api cool down for this long.
         /// </summary>
-        public const uint API_RATE_LIMIT_RESET_MS = 61000;
+        public static readonly uint API_RATE_LIMIT_RESET_MS = 61000;
 
-        public const string API_BASE_URL = "https://api.coingecko.com";
-        public const string API_PRO_BASE_URL = "https://pro-api.coingecko.com";
-        public const uint API_VERSION = 3;
+        public static readonly string API_BASE_URL = "https://api.coingecko.com";
+        public static readonly string API_PRO_BASE_URL = "https://pro-api.coingecko.com";
+        public static readonly uint API_VERSION = 3;
     }
 }
