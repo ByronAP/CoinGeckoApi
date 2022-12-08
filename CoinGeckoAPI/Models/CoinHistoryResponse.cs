faf43bd352207d96ac82f6581e09f80144f2ba95
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-08 14:52:04 -0500

    add

---
+++ CoinGeckoAPI/Models/CoinHistoryResponse.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CoinGeckoAPI.Models
+{
+    public class CoinHistoryResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("localization")]
+        public Dictionary<string, string> Localization { get; set; }
+
+        [JsonProperty("image")]
+        public CoinImage Image { get; set; }
+
+        [JsonProperty("market_data")]
+        public CoinMarketDataSlim MarketData { get; set; }
+
+        [JsonProperty("community_data")]
+        public CoinCommunityData CommunityData { get; set; }
+
+        [JsonProperty("developer_data")]
+        public CoinDeveloperData DeveloperData { get; set; }
+
+        [JsonProperty("public_interest_stats")]
+        public CoinPublicInterestStats PublicInterestStats { get; set; }
+    }
+}
