f8d3f0b21ff3a7a5bbc012928a7048ac2adcc20e
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-08 22:19:48 -0500

    documenting

--- CoinGeckoAPI/Constants.cs
+++ CoinGeckoAPI/Constants.cs
@@ -1,4 +1,4 @@
-using CoinGeckoAPI.Properties;
+﻿using CoinGeckoAPI.Properties;
 
 namespace CoinGeckoAPI
 {
@@ -10,17 +10,30 @@ namespace CoinGeckoAPI
         public static readonly string API_NAME = "CoinGecko";
 
         /// <summary>
-        /// Actual rate limit is 10-50 RPM
+        /// Actual rate limit is 10-50 RPM.
+        /// This is the recommended wait time between calls to not ever get rate limited.
         /// </summary>
         public static readonly uint API_RATE_LIMIT_MS = 6000;
+
         /// <summary>
         /// Time interval when the rate limit is reset.
-        /// AKA: If a response is 429 then let the api cool down for this long.
+        /// AKA: If a response code is 429 then let the api cool down for this long.
         /// </summary>
         public static readonly uint API_RATE_LIMIT_RESET_MS = 61000;
 
+        /// <summary>
+        /// The base API URL.
+        /// </summary>
         public static readonly string API_BASE_URL = "https://api.coingecko.com";
+
+        /// <summary>
+        /// The base URL for PRO feature calls.
+        /// </summary>
         public static readonly string API_PRO_BASE_URL = "https://pro-api.coingecko.com";
+
+        /// <summary>
+        /// The API version.
+        /// </summary>
         public static readonly uint API_VERSION = 3;
 
         /// <summary>
