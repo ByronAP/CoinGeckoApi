5e439c4181bc170958853cadab3cc1c4634dceaa
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-09 00:35:59 -0500

    add

---
+++ CoinGeckoAPI/Models/MarketChartCombinedItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoinGeckoAPI.Models
+{
+    public class MarketChartCombinedItem
+    {
+        public long TimestampMs { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal? MarketCap { get; set; }
+
+        public decimal? TotalVolume { get; set; }
+    }
+}
