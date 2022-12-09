1f47fd40ff4653bc3d6afa2228537277bfe6fff8
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-09 12:41:47 -0500

    add

---
+++ CoinGeckoAPI/Models/CoinContractResponse.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace CoinGeckoAPI.Models
+{
+    public class CoinContractResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("asset_platform_id")]
+        public string AssetPlatformId { get; set; }
+
+        [JsonProperty("platforms")]
+        public Dictionary<string, string> Platforms { get; set; }
+
+        [JsonProperty("detail_platforms")]
+        public Dictionary<string, CoinPlatformDetail> DetailPlatforms { get; set; }
+
+        [JsonProperty("block_time_in_minutes")]
+        public uint? BlockTimeInMinutes { get; set; }
+
+        [JsonProperty("hashing_algorithm")]
+        public object HashingAlgorithm { get; set; }
+
+        [JsonProperty("categories")]
+        public string[] Categories { get; set; }
+
+        [JsonProperty("public_notice")]
+        public object PublicNotice { get; set; }
+
+        [JsonProperty("additional_notices")]
+        public object[] AdditionalNotices { get; set; }
+
+        [JsonProperty("localization")]
+        public Dictionary<string, string> Localization { get; set; }
+
+        [JsonProperty("description")]
+        public Dictionary<string, string> Description { get; set; }
+
+        [JsonProperty("links")]
+        public CoinLinks Links { get; set; }
+
+        [JsonProperty("image")]
+        public CoinImage Image { get; set; }
+
+        [JsonProperty("country_origin")]
+        public string CountryOrigin { get; set; }
+
+        [JsonProperty("genesis_date")]
+        public DateTimeOffset? GenesisDate { get; set; }
+
+        [JsonProperty("contract_address")]
+        public string ContractAddress { get; set; }
+
+        [JsonProperty("sentiment_votes_up_percentage")]
+        public double? SentimentVotesUpPercentage { get; set; }
+
+        [JsonProperty("sentiment_votes_down_percentage")]
+        public double? SentimentVotesDownPercentage { get; set; }
+
+        [JsonProperty("market_cap_rank")]
+        public uint? MarketCapRank { get; set; }
+
+        [JsonProperty("coingecko_rank")]
+        public uint? CoingeckoRank { get; set; }
+
+        [JsonProperty("coingecko_score")]
+        public double? CoingeckoScore { get; set; }
+
+        [JsonProperty("developer_score")]
+        public double? DeveloperScore { get; set; }
+
+        [JsonProperty("community_score")]
+        public double? CommunityScore { get; set; }
+
+        [JsonProperty("liquidity_score")]
+        public double? LiquidityScore { get; set; }
+
+        [JsonProperty("public_interest_score")]
+        public double? PublicInterestScore { get; set; }
+
+        [JsonProperty("market_data")]
+        public CoinMarketData MarketData { get; set; }
+
+        [JsonProperty("community_data")]
+        public CoinCommunityData CommunityData { get; set; }
+
+        [JsonProperty("developer_data")]
+        public CoinDeveloperData DeveloperData { get; set; }
+
+        [JsonProperty("public_interest_stats")]
+        public CoinPublicInterestStats PublicInterestStats { get; set; }
+
+        [JsonProperty("status_updates")]
+        public object[] StatusUpdates { get; set; }
+
+        [JsonProperty("last_updated")]
+        public DateTimeOffset? LastUpdated { get; set; }
+
+        [JsonProperty("tickers")]
+        public CoinTicker[] Tickers { get; set; }
+    }
+}
---
+++ Tests/CoinsContractTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests
+{
+    internal class CoinsContractTests
+    {
+        private CoinGeckoClient _apiClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            _apiClient = new CoinGeckoClient();
+        }
+
+        [Test]
+        public async Task GetCoinContractTest()
+        {
+            await Helpers.DoRateLimiting();
+
+            var contractResult = await _apiClient.Coins.Contract.GetCoinContractAsync("ethereum", "0x514910771af9ca656af840dff83e8264ecf986ca");
+
+            Assert.That(contractResult, Is.Not.Null);
+            Assert.That(contractResult.Id, Is.EqualTo("chainlink"));
+        }
+    }
+}
