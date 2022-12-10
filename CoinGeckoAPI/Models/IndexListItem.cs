3e8b41768535a382db7284d83ee67a1741291fca
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 08:42:22 -0500

    add

---
+++ CoinGeckoAPI/Models/IndexListItem.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace CoinGeckoAPI.Models
+{
+    public class IndexListItem
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
