92201ef4a738c31954732fa285a4a6ae5f6d1ebf
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 00:14:48 -0500

    add

---
+++ CoinGeckoAPI/Models/ExchangeVolumeChartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace CoinGeckoAPI.Models
+{
+    public class ExchangeVolumeChartItem
+    {
+        public long? Timestamp { get; set; }
+        public decimal? volume { get; set; }
+    }
+}
