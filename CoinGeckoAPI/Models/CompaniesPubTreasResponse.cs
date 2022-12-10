9535b3d6d08e1eda04d7009b7c3dbc49543175b2
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 12:07:24 -0500

    add

---
+++ CoinGeckoAPI/Models/CompaniesPubTreasResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace CoinGeckoAPI.Models
+{
+    public class CompaniesPubTreasResponse
+    {
+        [JsonProperty("total_holdings")]
+        public decimal? TotalHoldings { get; set; }
+
+        [JsonProperty("total_value_usd")]
+        public decimal? TotalValueUsd { get; set; }
+
+        [JsonProperty("market_cap_dominance")]
+        public decimal? MarketCapDominance { get; set; }
+
+        [JsonProperty("companies")]
+        public CompanyInfo[] Companies { get; set; }
+    }
+}
