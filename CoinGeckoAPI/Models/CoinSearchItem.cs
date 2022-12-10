5b90645f26ae20366fcebe00532bc191d2182860
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 11:22:20 -0500

    add

---
+++ CoinGeckoAPI/Models/CoinSearchItem.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace CoinGeckoAPI.Models
+{
+    public class CoinSearchItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("api_symbol")]
+        public string ApiSymbol { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("market_cap_rank")]
+        public int? MarketCapRank { get; set; }
+
+        [JsonProperty("thumb")]
+        public string Thumb { get; set; }
+
+        [JsonProperty("large")]
+        public string Large { get; set; }
+    }
+}
