6790f71e96e2eaeb13a072517d9f0536f5e1f730
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 11:48:50 -0500

    add

---
+++ CoinGeckoAPI/Models/GlobalData.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CoinGeckoAPI.Models
+{
+    public class GlobalData
+    {
+        [JsonProperty("active_cryptocurrencies")]
+        public int? ActiveCryptocurrencies { get; set; }
+
+        [JsonProperty("upcoming_icos")]
+        public int? UpcomingIcos { get; set; }
+
+        [JsonProperty("ongoing_icos")]
+        public int? OngoingIcos { get; set; }
+
+        [JsonProperty("ended_icos")]
+        public int? EndedIcos { get; set; }
+
+        [JsonProperty("markets")]
+        public int? Markets { get; set; }
+
+        [JsonProperty("total_market_cap")]
+        public Dictionary<string, decimal> TotalMarketCap { get; set; }
+
+        [JsonProperty("total_volume")]
+        public Dictionary<string, decimal> TotalVolume { get; set; }
+
+        [JsonProperty("market_cap_percentage")]
+        public Dictionary<string, decimal> MarketCapPercentage { get; set; }
+
+        [JsonProperty("market_cap_change_percentage_24h_usd")]
+        public decimal? MarketCapChangePercentage24HUsd { get; set; }
+
+        [JsonProperty("updated_at")]
+        public long? UpdatedAt { get; set; }
+    }
+}
---
+++ CoinGeckoAPI/Models/GlobalResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CoinGeckoAPI.Models
+{
+    public class GlobalResponse
+    {
+        [JsonProperty("data")]
+        public GlobalData Data { get; set; }
+    }
+}
