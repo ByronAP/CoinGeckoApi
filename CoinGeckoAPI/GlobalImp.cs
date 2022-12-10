2c8375adf0dd3317268dd2e3d46df4021aaa11f7
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-10 11:56:39 -0500

    add Global GetGlobalDefiAsync

--- CoinGeckoAPI/GlobalImp.cs
+++ CoinGeckoAPI/GlobalImp.cs
@@ -29,5 +29,18 @@ namespace CoinGeckoAPI
 
             return JsonConvert.DeserializeObject<GlobalResponse>(jsonStr);
         }
+
+        /// <summary>
+        /// TODO: Document this.
+        /// </summary>
+        /// <returns>A Task&lt;GlobalDefiResponse&gt; representing the asynchronous operation.</returns>
+        public async Task<GlobalDefiResponse> GetGlobalDefiAsync()
+        {
+            var request = new RestRequest(CoinGeckoClient.BuildUrl("global", "decentralized_finance_defi"));
+
+            var jsonStr = await CoinGeckoClient.GetStringResponseAsync(_restClient, request, _logger);
+
+            return JsonConvert.DeserializeObject<GlobalDefiResponse>(jsonStr);
+        }
     }
 }
--- Tests/GlobalTests.cs
+++ Tests/GlobalTests.cs
@@ -11,7 +11,7 @@
         }
 
         [Test]
-        public async Task GetGlobalsTest()
+        public async Task GetGlobalTest()
         {
             await Helpers.DoRateLimiting();
 
@@ -20,6 +20,18 @@
             Assert.That(globalResult, Is.Not.Null);
             Assert.That(globalResult.Data, Is.Not.Null);
             Assert.That(globalResult.Data.ActiveCryptocurrencies, Is.GreaterThan(6000));
+        }
+
+        [Test]
+        public async Task GetGlobalDefiTest()
+        {
+            await Helpers.DoRateLimiting();
+
+            var globalResult = await _apiClient.Global.GetGlobalDefiAsync();
+
+            Assert.That(globalResult, Is.Not.Null);
+            Assert.That(globalResult.Data, Is.Not.Null);
+            Assert.That(Convert.ToDecimal(globalResult.Data.DefiDominance), Is.GreaterThan(1.1m));
         }
     }
 }
