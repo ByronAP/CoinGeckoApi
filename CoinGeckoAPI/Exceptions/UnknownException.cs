590b07eeb7b3be84509c3d073b37aa3da45470cc
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-11 14:02:50 -0500

    documenting

--- CoinGeckoAPI/Exceptions/UnknownException.cs
+++ CoinGeckoAPI/Exceptions/UnknownException.cs
@@ -2,6 +2,11 @@
 
 namespace CoinGeckoAPI.Exceptions
 {
+    /// <summary>
+    /// An exception because I am dumb and don't know WFT it is.
+    /// Implements the <see cref="Exception" />
+    /// </summary>
+    /// <seealso cref="Exception" />
     public class UnknownException : Exception
     {
         public UnknownException(string message) : base(message)
--- CoinGeckoAPI/Types/NftsListOrderBy.cs
+++ CoinGeckoAPI/Types/NftsListOrderBy.cs
@@ -3,13 +3,37 @@
     public enum NftsListOrderBy
     {
         None,
+        /// <summary>
+        /// Native currency volume 24 hours ascending
+        /// </summary>
         h24_volume_native_asc,
+        /// <summary>
+        /// Native currency volume 24 hours descending
+        /// </summary>
         h24_volume_native_desc,
+        /// <summary>
+        /// Native currency price floor ascending
+        /// </summary>
         floor_price_native_asc,
+        /// <summary>
+        /// Native currency price floor descending
+        /// </summary>
         floor_price_native_desc,
+        /// <summary>
+        /// Native currency market cap ascending
+        /// </summary>
         market_cap_native_asc,
+        /// <summary>
+        /// Native currency market cap descending
+        /// </summary>
         market_cap_native_desc,
+        /// <summary>
+        /// USD market cap ascending
+        /// </summary>
         market_cap_usd_asc,
+        /// <summary>
+        /// USD market cap descending
+        /// </summary>
         market_cap_usd_desc
     }
 }
