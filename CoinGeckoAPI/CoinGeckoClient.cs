2258ace63d57d074987779f1b21509222ea342f4
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-11 15:37:27 -0500

    4 complete documentation (#9)
    * documenting
    
    * WIP
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * documenting
    
    * fix call name
    
    * add
    
    * add imgs to sln
    
    * update readme

--- CoinGeckoAPI/CoinGeckoClient.cs
+++ CoinGeckoAPI/CoinGeckoClient.cs
@@ -1,4 +1,16 @@
-﻿using CoinGeckoAPI.Exceptions;
+﻿// ***********************************************************************
+// Assembly         : CoinGeckoAPI
+// Author           : ByronAP
+// Created          : 12-10-2022
+//
+// Last Modified By : ByronAP
+// Last Modified On : 12-11-2022
+// ***********************************************************************
+// <copyright file="CoinGeckoClient.cs" company="ByronAP">
+//     Copyright © 2022 ByronAP, CoinGecko. All rights reserved.
+// </copyright>
+// ***********************************************************************
+using CoinGeckoAPI.Exceptions;
 using CoinGeckoAPI.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -10,68 +22,92 @@ using System.Threading.Tasks;
 
 namespace CoinGeckoAPI
 {
+    /// <summary>
+    /// <para>Create an instance of this class to access the API methods.</para>
+    /// <para>
+    /// Methods and parameters are named as specified in the official
+    /// CoinGecko API documentation (Ex: API call '/coins/list' 
+    /// translates to 'CoinGeckoClient.Coins.GetCoinsListAsync()').
+    /// </para>
+    /// </summary>
     public class CoinGeckoClient
     {
         /// <summary>
-        /// The RestSharp client instance.
-        /// This is exposed in case you wish to change the options such as use a proxy.
+        /// The RestSharp client instance used to make the API calls.
+        /// This is exposed in case you wish to change options such as use a proxy.
         /// </summary>
+        /// <value>The RestSharp client instance.</value>
         public RestClient CGRestClient { get; }
 
         /// <summary>
-        /// Simple API calls.
+        /// <para>Provides access to the Simple API calls.</para>
+        /// An instance of <see cref="SimpleImp"/>.
         /// </summary>
+        /// <value>Simple API calls.</value>
         public SimpleImp Simple { get; }
 
         /// <summary>
-        /// Coins API calls.
+        /// <para>Provides access to the Coins API calls.</para>
+        /// An instance of <see cref="CoinsImp"/>.
         /// </summary>
+        /// <value>Coins API calls.</value>
         public CoinsImp Coins { get; }
 
         /// <summary>
-        /// Exchanges API calls.
+        /// <para>Provides access to the Exchanges API calls.</para>
+        /// An instance of <see cref="ExchangesImp"/>.
         /// </summary>
+        /// <value>Exchanges API calls.</value>
         public ExchangesImp Exchanges { get; }
 
         /// <summary>
-        /// Index API calls.
+        /// <para>Provides access to the Indexes API calls.</para>
+        /// An instance of <see cref="IndexesImp"/>.
         /// </summary>
-        /// <value>The indexes.</value>
+        /// <value>Indexes API calls.</value>
         public IndexesImp Indexes { get; }
 
         /// <summary>
-        /// Derivatives API calls.
+        /// <para>Provides access to the Derivatives API calls.</para>
+        /// An instance of <see cref="DerivativesImp"/>.
         /// </summary>
-        /// <value>The derivatives.</value>
+        /// <value>Derivatives API calls.</value>
         public DerivativesImp Derivatives { get; }
 
         /// <summary>
-        /// NFTS API calls.
+        /// <para>Provides access to the Nfts API calls.</para>
+        /// An instance of <see cref="NftsImp"/>.
         /// </summary>
-        /// <value>The NFTS.</value>
+        /// <value>Nfts API calls.</value>
         public NftsImp Nfts { get; }
 
         /// <summary>
-        /// Search API calls.
+        /// <para>Provides access to the Search API calls.</para>
+        /// An instance of <see cref="SearchImp"/>.
         /// </summary>
-        /// <value>The search.</value>
+        /// <value>Search API calls.</value>
         public SearchImp Search { get; }
 
         /// <summary>
-        /// Global API calls.
+        /// <para>Provides access to the Global API calls.</para>
+        /// An instance of <see cref="GlobalImp"/>.
         /// </summary>
-        /// <value>The global.</value>
+        /// <value>Global API calls.</value>
         public GlobalImp Global { get; }
 
         /// <summary>
-        /// Companies API calls.
+        /// <para>Provides access to the Companies API calls.</para>
+        /// An instance of <see cref="CompaniesImp"/>.
         /// </summary>
-        /// <value>The companies.</value>
+        /// <value>Companies API calls.</value>
         public CompaniesImp Companies { get; }
 
         private readonly ILogger<CoinGeckoClient> _logger;
 
         #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoinGeckoClient"/> class.
+        /// </summary>
         public CoinGeckoClient()
         {
             _logger = null;
@@ -89,6 +125,10 @@ namespace CoinGeckoAPI
             Companies = new CompaniesImp(CGRestClient, _logger);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoinGeckoClient"/> class.
+        /// </summary>
+        /// <param name="isPro">if set to <c>true</c> [is pro].</param>
         public CoinGeckoClient(bool isPro)
         {
             _logger = null;
@@ -113,6 +153,10 @@ namespace CoinGeckoAPI
             Companies = new CompaniesImp(CGRestClient, _logger);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoinGeckoClient"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public CoinGeckoClient(ILogger<CoinGeckoClient> logger)
         {
             _logger = logger;
@@ -130,6 +174,11 @@ namespace CoinGeckoAPI
             Companies = new CompaniesImp(CGRestClient, _logger);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoinGeckoClient"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="isPro">if set to <c>true</c> [is pro].</param>
         public CoinGeckoClient(ILogger<CoinGeckoClient> logger, bool isPro)
         {
             _logger = logger;
@@ -228,7 +277,7 @@ namespace CoinGeckoAPI
         /// <summary>
         /// Get BTC-to-Currency exchange rates.
         /// </summary>
-        /// <returns>List of rates.</returns>
+        /// <returns>An instance of <see cref="ExchangeRatesResponse"/></returns>
         public async Task<ExchangeRatesResponse> GetExchangeRatesAsync()
         {
             var request = new RestRequest(BuildUrl("exchange_rates"));
@@ -242,7 +291,7 @@ namespace CoinGeckoAPI
         /// List all asset platforms (Blockchain networks).
         /// </summary>
         /// <param name="filter">Apply relevant filters to results. Valid values: "nft" (asset_platform nft-support).</param>
-        /// <returns>List all asset_platforms.</returns>
+        /// <returns>List all asset_platforms. <see cref="AssetPlatform"/></returns>
         public async Task<IEnumerable<AssetPlatform>> GetAssetPlatformsAsync(string filter = null)
         {
             var request = new RestRequest(BuildUrl("asset_platforms"));
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # CoinGeckoApi
-CoinGecko API Library for .NET
+![CoinGecko Logo](https://raw.githubusercontent.com/ByronAP/CoinGeckoApi/dev/coingecko-logo-banner-256x64.png)
+## CoinGecko API Library for .NET
 
 ![Nuget](https://img.shields.io/nuget/v/CoinGeckoAPI)
-[![coingeckoapi on fuget.org](https://www.fuget.org/packages/coingeckoapi/badge.svg)](https://www.fuget.org/packages/coingeckoapi)
+
 
 [![Codacy Badge](https://app.codacy.com/project/badge/Grade/11157f3b39e84f0c9a0a1bc0caf148dc)](https://www.codacy.com/gh/ByronAP/CoinGeckoApi/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ByronAP/CoinGeckoApi&amp;utm_campaign=Badge_Grade)
 [![CodeQL](https://github.com/ByronAP/CoinGeckoApi/actions/workflows/codeql.yml/badge.svg)](https://github.com/ByronAP/CoinGeckoApi/actions/workflows/codeql.yml)
 [![Tests](https://github.com/ByronAP/CoinGeckoApi/actions/workflows/dev_test_dotnet.yml/badge.svg)](https://github.com/ByronAP/CoinGeckoApi/actions/workflows/dev_test_dotnet.yml)
-
 
 [CoinGecko API Documentation](https://www.coingecko.com/en/api/documentation)
 
