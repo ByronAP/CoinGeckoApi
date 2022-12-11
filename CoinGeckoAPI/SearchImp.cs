e89236163190dd358295e36c46ecccbbbe836ea8
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-11 13:38:44 -0500

    documenting

--- CoinGeckoAPI/SearchImp.cs
+++ CoinGeckoAPI/SearchImp.cs
@@ -1,4 +1,16 @@
-﻿using CoinGeckoAPI.Models;
+﻿// ***********************************************************************
+// Assembly         : CoinGeckoAPI
+// Author           : ByronAP
+// Created          : 12-10-2022
+//
+// Last Modified By : ByronAP
+// Last Modified On : 12-11-2022
+// ***********************************************************************
+// <copyright file="SearchImp.cs" company="ByronAP">
+//     Copyright © 2022 ByronAP, CoinGecko. All rights reserved.
+// </copyright>
+// ***********************************************************************
+using CoinGeckoAPI.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
@@ -7,6 +19,11 @@ using System.Threading.Tasks;
 
 namespace CoinGeckoAPI
 {
+    /// <summary>
+    /// <para>Implementation of the '/search' API calls.</para>
+    /// <para>Implementation classes do not have a public constructor
+    /// and must be accessed through an instance of <see cref="CoinGeckoClient"/>.</para>
+    /// </summary>
     public class SearchImp
     {
         private readonly RestClient _restClient;
@@ -19,10 +36,10 @@ namespace CoinGeckoAPI
         }
 
         /// <summary>
-        /// TODO: Document this.
+        /// Search for coins, categories and markets listed on CoinGecko ordered by largest Market Cap first.
         /// </summary>
-        /// <param name="query">The query.</param>
-        /// <returns>SearchResponse.</returns>
+        /// <param name="query">The search query string.</param>
+        /// <returns>A Task&lt;<see cref="SearchResponse"/>&gt; representing the asynchronous operation.</returns>
         /// <exception cref="System.ArgumentNullException">query - Invalid value. Value must be a valid search term.</exception>
         public async Task<SearchResponse> GetSearchAsync(string query)
         {
@@ -40,9 +57,10 @@ namespace CoinGeckoAPI
         }
 
         /// <summary>
-        /// TODO: Document this.
+        /// Get trending search coins (Top-7) on CoinGecko in the last 24 hours.
+        /// <para>Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first).</para>
         /// </summary>
-        /// <returns>A Task&lt;SearchTrendingResponse&gt; representing the asynchronous operation.</returns>
+        /// <returns>A Task&lt;<see cref="SearchTrendingResponse"/>&gt; representing the asynchronous operation.</returns>
         public async Task<SearchTrendingResponse> GetSearchTrendingAsync()
         {
             var request = new RestRequest(CoinGeckoClient.BuildUrl("search", "trending"));
