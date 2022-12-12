fe6d8ccb61fd1a0028469ab3d923dfd07acade7d
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-12 13:33:50 -0500

    replace duplicate code with call

--- CoinGeckoAPI/ExchangesImp.cs
+++ CoinGeckoAPI/ExchangesImp.cs
@@ -153,19 +153,7 @@ namespace CoinGeckoAPI
         /// <exception cref="System.ArgumentNullException">id - Invalid value. Value must be a valid exchange id (EX: bitstamp)</exception>
         public async Task<IEnumerable<ExchangeVolumeChartItem>> GetExchangeVolumeChartFriendlyAsync(string id, uint days)
         {
-            if (string.IsNullOrEmpty(id) || String.IsNullOrWhiteSpace(id))
-            {
-                throw new ArgumentNullException(nameof(id), "Invalid value. Value must be a valid exchange id (EX: bitstamp)");
-            }
-
-            if (days == 0) { days = 1; }
-
-            var request = new RestRequest(CoinGeckoClient.BuildUrl("exchanges", id, "volume_chart"));
-            request.AddQueryParameter("days", days);
-
-            var jsonStr = await CoinGeckoClient.GetStringResponseAsync(_restClient, request, _cache, _logger);
-
-            var data = JsonConvert.DeserializeObject<string[][]>(jsonStr);
+            var data = await GetExchangeVolumeChartAsync(id, days);
 
             // the format of data suxxx so we automatically make it more easily used
             // by transforming it into concrete class enumerable
