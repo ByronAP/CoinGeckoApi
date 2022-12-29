09dc70ecb1b88a6ab23cc98f9ea90926b3332ff2
Allen Byron Penner <7310089+ByronAP@users.noreply.github.com>
2022-12-29 13:32:42 -0500

    add

---
+++ CoinGeckoAPI/CoinGeckoServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CoinGeckoAPI;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CoinGeckoServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCoinGeckoApi(this IServiceCollection services)
+            => services.AddSingleton<CoinGeckoClient>();
+
+        public static IServiceCollection AddCoinGeckoApi(this IServiceCollection services, string apiKey)
+            => services.AddSingleton<CoinGeckoClient>(new CoinGeckoClient(apiKey: apiKey));
+
+        public static IServiceCollection AddCoinpaprikaApi(this IServiceCollection services, ILogger<CoinGeckoClient> logger)
+            => services.AddSingleton<CoinGeckoClient>(new CoinGeckoClient(logger: logger));
+
+        public static IServiceCollection AddCoinpaprikaApi(this IServiceCollection services, string apiKey, ILogger<CoinGeckoClient> logger)
+            => services.AddSingleton<CoinGeckoClient>(new CoinGeckoClient(apiKey: apiKey, logger: logger));
+    }
+}
